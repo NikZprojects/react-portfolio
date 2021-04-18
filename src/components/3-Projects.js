19943d2f51de34496b1011f32626b397494daaf4
NikZprojects <nizprojects@gmail.com>
2021-04-18 11:45:17 -0400

    Added all projects and styling for them.

--- src/components/3-Projects.js
+++ src/components/3-Projects.js
@@ -1,9 +1,48 @@
 import Project from "./Project";
 
+const projects = [
+  {
+    name: "React Habit Tracker",
+    description:
+      "A habit tracker built with the ability to save data thanks to Google authentication, an Express API, and a MongoDB database.",
+    imgsrc: "/img/reacthabittracker.jpg",
+    github: "https://github.com/NikZprojects/React-Habit-Tracker",
+    link: "https://www.nikzprojects.com/React_Habit_Tracker",
+  },
+  {
+    name: "Instagram Search Bar",
+    description:
+      "A single page React app search bar built to search through a friend's Instagram recipes. Node backend makes calls to the Instagram Basic Display API to keep everything up-to-date.",
+    imgsrc: "img/instagram.jpg",
+    github: "https://github.com/NikZprojects/Instagram-Search-Bar",
+    link: "https://instagram.nikzprojects.com/",
+  },
+  {
+    name: "Food Blog Website",
+    description:
+      "A full-stack Django website built to share my recipes. A responsive website that Bootstrap to style and scale the elements on the page.",
+    imgsrc: "img/cookwithchemistry.jpg",
+    github: "https://github.com/NikZprojects/Food-Blog-Django",
+    link: "https://www.cookwithchemistry.com",
+  },
+];
+
 const Projects = () => (
   <div>
     <h1>Projects</h1>
-    <Project />
+    {projects.map((p) => {
+      return (
+        <Project
+          className="project"
+          key={p.name}
+          name={p.name}
+          description={p.description}
+          imgsrc={p.imgsrc}
+          github={p.github}
+          link={p.link}
+        />
+      );
+    })}
   </div>
 );
 
