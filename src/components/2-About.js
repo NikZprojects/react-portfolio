b73c3ec05c0971e80d0a6aeaafaaa192a526e123
NikZprojects <nizprojects@gmail.com>
2021-04-17 21:01:10 -0400

    Edit about for textbox class

--- src/components/2-About.js
+++ src/components/2-About.js
@@ -1,22 +1,25 @@
 const About = () => (
   <div>
     <h3>Hi there. I'm Nik.</h3>
-    <p>
-      I started programming in late 2019 as a way to diversify my current
-      skillset. It didn't take long before I found a passion for building things
-      online.
-    </p>
-    <p>
-      I first built a simple Flask website to share some of my Python scripts in
-      a web format. From there, I designed and built a food blog with Django at
-      CookwithChemistry.com
-    </p>
-    <p>
-      From there I built a single page React app to search through a friend's
-      Instagram page. Most recently I designed and built a habit tracker with
-      the MERN (MongoDB, Express, React, Node) stack, as a way to track my own
-      habits and share how I do so with others.
-    </p>
+    <h1>About</h1>
+    <div className="textblock">
+      <p>
+        I started programming in late 2019 as a way to diversify my current
+        skillset. It didn't take long before I found a passion for building
+        things online.
+      </p>
+      <p>
+        I first built a simple Flask website to share some of my Python scripts
+        in a web format. From there, I designed and built a food blog with
+        Django at CookwithChemistry.com
+      </p>
+      <p>
+        From there I built a single page React app to search through a friend's
+        Instagram page. Most recently I designed and built a habit tracker with
+        the MERN (MongoDB, Express, React, Node) stack, as a way to track my own
+        habits and share how I do so with others.
+      </p>
+    </div>
   </div>
 );
 
